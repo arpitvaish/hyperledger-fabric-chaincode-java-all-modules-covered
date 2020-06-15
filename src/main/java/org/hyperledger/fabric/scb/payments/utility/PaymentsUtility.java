b5c5fb2f06c575c944299cc255e43aee6519f4af
Arpit <arpitvaish@hotmail.com>
2020-06-15 11:17:11 +0530

    Create PaymentsUtility.java

---
+++ src/main/java/org/hyperledger/fabric/scb/payments/utility/PaymentsUtility.java
@@ -0,0 +1,22 @@
+package org.hyperledger.fabric.scb.payments.utility;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.List;
+
+/**
+ * Created by Arpit Vaish on 10/18/2019.
+ */
+public final class PaymentsUtility {
+    private static Log logger = LogFactory.getLog(PaymentsUtility.class);
+
+    private PaymentsUtility() {
+    }
+    public static boolean validateCurrency(final List<String> validCurrency, final String inputCurrency) {
+        return validCurrency.contains(inputCurrency.toUpperCase())
+                || validCurrency.contains(inputCurrency.toLowerCase());
+    }
+
+
+}
