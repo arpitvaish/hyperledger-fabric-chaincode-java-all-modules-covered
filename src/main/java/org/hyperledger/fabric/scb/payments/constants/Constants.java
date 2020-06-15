7f31d37b39a21660e2d2479879ce800b89db4aed
Arpit <arpitvaish@hotmail.com>
2020-06-15 11:18:09 +0530

    Create Constants.java

---
+++ src/main/java/org/hyperledger/fabric/scb/payments/constants/Constants.java
@@ -0,0 +1,21 @@
+package org.hyperledger.fabric.scb.payments.constants;
+
+/**
+ * Created by 1604993 on 10/30/2019.
+ */
+public final class Constants {
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+    public static final int FOUR = 4;
+    public static final int FIVE = 5;
+    public static final int SIX = 6;
+    public static final int SEVEN = 7;
+    public static final int EIGHT = 8;
+    public static final int NINE = 9;
+    public static final int TEN = 10;
+    public static final int ELEVEN = 11;
+    private Constants() {
+    }
+}
